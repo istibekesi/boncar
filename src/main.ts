a88d58cccdff7f23bd65341775d1ad13d5b67f52
istibekesi <istibekesi@gmail.com>
2016-06-27 13:47:33 +0200

    Add firebase

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  FIREBASE_PROVIDERS,
+  // Initialize Firebase app
+  defaultFirebase({
+    apiKey: "AIzaSyC2SF0T6cJUym7zbIlUfo2kq6kDMXgjrRc",
+    authDomain: "project-8652230604760829170.firebaseapp.com",
+    databaseURL: "https://project-8652230604760829170.firebaseio.com",
+    storageBucket: "project-8652230604760829170.appspot.com",
+  })
+]);
 
