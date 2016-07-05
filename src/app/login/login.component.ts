9caaebe38e117b553f2116b8d8fca81e689ba99e
istibekesi <istibekesi@gmail.com>
2016-07-05 15:46:15 +0200

    Login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ROUTER_DIRECTIVES } from "@angular/router";
 
 @Component({
   moduleId: module.id,
   selector: 'app-login',
   templateUrl: 'login.component.html',
-  styleUrls: ['login.component.css']
+  styleUrls: ['login.component.css'],
+  directives: [ ROUTER_DIRECTIVES ]
 })
 export class LoginComponent implements OnInit {
 
   constructor() {}
 
   ngOnInit() {
+  }
+
+  onLoginFormSubmit(formObject) {
+    console.log(formObject);
   }
 
 }
