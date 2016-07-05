84a7760e8d85812a4b694d300ae8da4789b8afcb
istibekesi <istibekesi@gmail.com>
2016-07-05 17:53:48 +0200

    Try to email/pass login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ROUTER_DIRECTIVES } from "@angular/router";
+import { AngularFire, AuthProviders, AuthMethods, FirebaseAuthState } from "angularfire2";
+import {EmailPasswordCredentials} from "angularfire2/es6/providers/auth_backend";
 
 @Component({
   moduleId: module.id,
@@ -10,13 +12,42 @@ import { ROUTER_DIRECTIVES } from "@angular/router";
 })
 export class LoginComponent implements OnInit {
 
-  constructor() {}
+  constructor(public af: AngularFire) {
+  }
 
   ngOnInit() {
   }
 
   onLoginFormSubmit(formObject) {
-    console.log(formObject);
+    //console.log("***" + formObject.controls.email.value);
+    //console.log("***" + formObject.controls.password.value);
+
+    console.log("Logging in email/password user...");
+
+    /*
+    this.af.auth.login({
+      provider: AuthProviders.Password,
+      method: AuthMethods.Password,
+    }).then(
+      success => console.log('Email login success: ' + success)
+    ).catch(
+      err => console.log('Email login FAILED: ' + err)
+    );
+    */
+
+    let c : EmailPasswordCredentials = {
+      email : formObject.controls.email.value,
+      password: formObject.controls.password.value
+    };
+
+    this.af.auth.login(
+      c
+    ).then(
+      success => console.log('Email login success: ' + success)
+    ).catch(
+      err => console.log('Email login FAILED: ' + err)
+    );
+
   }
 
 }
