5d610da86ec5fbdaf78aeb93e3e8bde9e46744d8
istibekesi <istibekesi@gmail.com>
2016-07-19 18:48:08 +0200

    Chat works

--- src/app/ride/ride.component.ts
+++ src/app/ride/ride.component.ts
@@ -21,20 +21,44 @@ import 'rxjs/add/operator/map';
       <div class="col-sm-8">
         <h2>Ride on {{day}}</h2>
         
-        <hr/>
         
-        Chat:<br/>
-        {{rideChat | json}}
+        <br/>
+        
+        <!--
+        <ul class="list-group">
+          <li *ngFor="let chatEntry of chatEntries" class="list-group-item">
+            <img class="img-circle pull-right" [src]="chatEntry.newsht?.avatarUrl" heigth="36" width="36">
+            {{chatEntry.msg}}
+          </li>
+        </ul>
+        -->
         
         
-        <div class="form-group">
-          <input type="text" [(ngModel)]="msg">
-          <button (click)="postChat()" class="btn btn-success">Send</button>
+        <h3>Chat</h3>
+        <div *ngFor="let chatEntry of chatEntries">
+            <img class="img-circle pull-left" [src]="chatEntry.newsht?.avatarUrl" heigth="36" width="36">
+            <p class="text-muted">{{chatEntry.newsht?.alias}}&nbsp;</p>
+            {{chatEntry.msg}}
         </div>
         
-        <hr/><hr/><hr/>
+        <div>
+            <img class="img-circle pull-left" [src]="loginService.userDetail?.avatarUrl" heigth="36" width="36">
+            <p class="text-muted">{{loginService.userDetail?.alias}}&nbsp;</p>
+            <input type="text" [(ngModel)]="msg">
+            <button (click)="postChat()" class="btn btn-success">Send</button>
+        </div>
         
-        {{entries}}
+        <!--<div class="form-group">
+          <input type="text" [(ngModel)]="msg">
+          <button (click)="postChat()" class="btn btn-success">Send</button>
+        </div>-->
+        
+        <hr/>
+        
+        <!--
+        Chat:<br/>
+        {{chatEntries | json}}
+        -->
         
       </div>
       
@@ -52,8 +76,11 @@ export class RideComponent implements OnInit {
   rideChat : Array<any>;
   msg : string;
 
-  entries;
-
+  // singel object
+  //chatEntryObs;
+  //chatEntry;
+  chatEntriesObs;
+  chatEntries;
 
   constructor(private route: ActivatedRoute, private router: Router, public af: AngularFire, public loginService: LoginService) {
 
@@ -65,12 +92,36 @@ export class RideComponent implements OnInit {
       this.rideId = params['rideid'];
       this.rideObs = this.af.database.object('rides/' + this.day + '/' + this.rideId );
 
+
+      // this is how MAP add a property to a LIST object observer
+      //this.chatEntryObs = this.af.database.object('rides/' + this.day + '/' + this.rideId + '/chat/-KN2a5mMcGnSXFO8IsXd') // returns an object
+      //  .map( entry => {
+      //    entry.newsht = "XXX";
+      //    return entry;
+      //  });
+
+      // this is how MAP add a property to a LIST object observer
+      this.chatEntriesObs = this.af.database.list('rides/' + this.day + '/' + this.rideId + '/chat')  // returns an array
+        .map( chatEntries => {
+          return chatEntries.map( entry => {
+            this.af.database.object('userDetail/'+entry.userId).subscribe(
+              userDetail => {
+                entry.newsht = userDetail;
+              }
+            );
+            return entry;
+          })
+        });
+
+
     });
 
+    this.chatEntriesObs.subscribe( ce => {
+      this.chatEntries = ce;
+    });
 
     this.rideObs.subscribe( ride => {
       this.rideChat = ride.chat;
-
     });
 
   }
@@ -86,6 +137,8 @@ export class RideComponent implements OnInit {
     };
     let chat = this.af.database.list('rides/' + this.day + '/' + this.rideId + '/chat');
     chat.push(chatEntry);
+
+    this.msg = '';
 
   }
 
